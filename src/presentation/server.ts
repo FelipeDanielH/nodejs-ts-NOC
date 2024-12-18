b81ed627a93ebedb844ed15d339c6767ac68a5a2
Felipe <felipe.daniel.henriquez@gmail.com>
2024-12-18 10:48:01 -0300

    feat: CronService
    se agrega cron como dependencia y se prueba en el main. mas adelante se deberia cambiar

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,18 @@
+import { CronJob } from "cron";
 
 
 export class Server {
-    public static start(){
+    public static start() {
         console.log('Server started');
+
+        const job = new CronJob(
+            '*/30 * * * * *', // cronTime
+            function () {
+                console.log('You will see this message every 30 seconds');
+            }, // onTick
+            null, // onComplete
+            true, // start
+            'America/Los_Angeles' // timeZone
+        );
     }
 }
