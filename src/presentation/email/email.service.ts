f883d82f4e22cc8ce7587cdf24a8dc478bbeee44
Felipe <felipe.daniel.henriquez@gmail.com>
2024-12-21 16:18:25 -0300

    feat(mail): se agrega adjuntar archivos adjuntos

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -3,11 +3,16 @@ import { envs } from '../../config/plugins/envs.plugin'
 
 
 interface SendMailOptions {
-    to: string;
+    to: string | string[];
     subject: string;
     htmlBody: string;
+    attachments?: Attachement[];
 }
 
+interface Attachement {
+    filename: string;
+    path: string;
+}
 
 export class EmailService {
 
@@ -19,9 +24,11 @@ export class EmailService {
         }
     });
 
+
     async sendEmail(options: SendMailOptions): Promise<boolean> {
 
         const { to, subject, htmlBody } = options;
+
 
         try {
 
@@ -31,14 +38,47 @@ export class EmailService {
                 html: htmlBody
             })
 
-            console.log(sentInformation);
+            console.log('desde sendEmail: ',sentInformation);
 
             return true;
         } catch (error) {
+            console.log(`${error}`)
             return false;
         }
 
     }
 
+    async sendEmailWithFileSystemLogs(to: string | string[]): Promise<void> {
+
+        const subject = 'Logs del servidor'
+        const htmlBody = `<h1> LOGS </h1>
+            <h2> Sistema NOC </h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum eius explicabo dolores vitae, dignissimos iure illum, accusamus dolorum at rerum cum magni adipisci. Numquam eligendi consequatur pariatur ratione voluptatem.  </p>
+            <p>ver los logs adjuntos</p>
+            <hr>`
+
+
+        const attachments: Attachement[] = [
+            {
+                filename: 'logs-low.log',
+                path: 'logs/logs-low.log'
+            },
+            {
+                filename: 'logs-medium.log',
+                path: 'logs/logs-medium.log'
+            },
+            {
+                filename: 'logs-high.log',
+                path: 'logs/logs-high.log'
+            }
+        ]
+
+        const sentInformation = await this.transporter.sendMail({
+            to, subject, attachments, html: htmlBody
+        })
+
+        console.log('desde sendEmailWithFileSystemLogs: ',sentInformation);
+    }
 
 }
+
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -18,7 +18,11 @@ export class Server {
         //* MANDAR EMAIL
         const emailService = new EmailService();
 
-        emailService.sendEmail({
+        //* Con attachments
+        emailService.sendEmailWithFileSystemLogs('felipe.daniel.henriquez@gmail.com');
+
+        //* Sin attachments
+        /* emailService.sendEmail({
             to: 'felipe.daniel.henriquez@gmail.com',
             subject: 'logs de sistema',
             htmlBody: `<h1> LOGS </h1>
@@ -26,7 +30,7 @@ export class Server {
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. In earum eius explicabo dolores vitae, dignissimos iure illum, accusamus dolorum at rerum cum magni adipisci. Numquam eligendi consequatur pariatur ratione voluptatem.  </p>
             <p>ver los logs adjuntos</p>
             <hr>`
-        })
+        }) */
 
         /* CronService.createJob( 
             '* * * * * *', // Every minute
